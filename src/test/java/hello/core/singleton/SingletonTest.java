36f2c5dd680de6e4dfa6a6be3cdec8e6afcb638e
tlswlsdntks <tlswlsdntks@naver.com>
2022-12-20 17:11:07 +0900

    * springContainer

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -4,6 +4,8 @@ import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,7 +20,7 @@ public class SingletonTest {
         // 1. 조회: 호출할 때마다 객체를 생성
         MemberService memberService1 = appConfig.memberService();
 
-        // 1. 조회: 호출할 때마다 객체를 생성
+        // 2. 조회: 호출할 때마다 객체를 생성
         MemberService memberService2 = appConfig.memberService();
 
         // 참조값이 다른 것을 확인
@@ -44,5 +46,25 @@ public class SingletonTest {
         assertThat(singletonService1).isSameAs(singletonService2);
 
     }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1); // 555cf22
+        System.out.println("memberService2 = " + memberService2); // 555cf22
+
+        // memberService1 == memberService2
+        assertThat(memberService1).isSameAs(memberService2);
+
+    }
+
 
 }

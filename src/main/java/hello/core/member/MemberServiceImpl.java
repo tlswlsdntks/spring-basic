c053648508aca739a4ee92daf0ac936bb3997762
tlswlsdntks <tlswlsdntks@naver.com>
2022-12-20 19:30:00 +0900

    * AutoAppConfigTest

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,12 +3,15 @@ package hello.core.member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+//@Component("MemberServiceImpl")
 @Component
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
     @Autowired  // ac.getBean(MemberRepository.class)
+    // 타입따라 의존관계가 주입된다
+    // MemberRepository <- MemoryMemberRepository (상속관계, 같은타입)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }

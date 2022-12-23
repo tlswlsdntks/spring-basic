47b26acb62612d2462bec36e6ee28c76e285ae92
tlswlsdntks <tlswlsdntks@naver.com>
2022-12-23 18:27:57 +0900

    * 2개이상의 등록된 빈 조회, 변수(필드)명, 파라미터명으로 매칭, @Qualifier, @Primary, 우선순위: @Qualifier > @Primary

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,10 +8,11 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -25,22 +26,32 @@ public class OrderServiceImpl implements OrderService {
     private final DiscountPolicy discountPolicy;
 
 
-
     // 1) 생성자 주입
     //  1번만 호출!
     // "불변, 필수" 의존관계에 사용
     // 생성자가 한개만 있을 땐, @Autowired 생략이 가능하다
-//    @Autowired // 생략 가능
-//    @RequiredArgsConstructor
-//    Ctrl + F12로 메소드 존재 확인 가능
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-////        System.out.println("memberRepository = " + memberRepository);
-////        System.out.println("discountPolicy = " + discountPolicy);
-////        System.out.println("1. memberRepository = " + memberRepository);
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    //    @Autowired // 생략 가능
+    //    @RequiredArgsConstructor
+    //    Ctrl + F12로 메소드 존재 확인 가능
+    public OrderServiceImpl(MemberRepository memberRepository,
+//                            @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+                            DiscountPolicy discountPolicy) {
+//        System.out.println("memberRepository = " + memberRepository);
+//        System.out.println("discountPolicy = " + discountPolicy);
+//        System.out.println("1. memberRepository = " + memberRepository);
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+//        this.discountPolicy = rateDiscountPolicy;
+    }
 
+
+    // @Autowired 매칭정리
+    // 1. 타입 매칭
+    // 2. 2개 이상일 때는 필드명, 파라미터명으로 빈 이름 매칭
+    // 3. @Qualifier: @Component @Qualifier -> @Qualifier 매칭
+    // 4. @Primary: 여러개 빈 중 @Primary 하나만 매칭
+    // @Primary 보다
+    // 명시적으로 입력한 @Qualifier 가 우선순위가 더 높다
 
 
     // 2) 수정자 주입
@@ -62,7 +73,6 @@ public class OrderServiceImpl implements OrderService {
 //    }
 
 
-
     // 3) 자바빈 프로퍼티 규약 예시
     /*
         class {
@@ -72,13 +82,11 @@ public class OrderServiceImpl implements OrderService {
      */
 
 
-
     // 4) 필드 주입
     // 필드주입을 권장하지 않는다!
     // 테스트 코드에서만 활용가능 -> 그래도 사용을 권장하지 않는다!
 //    @Autowired private MemberRepository memberRepository;
 //    @Autowired private DiscountPolicy discountPolicy;
-
 
 
     // 5) 일반 메소드 주입
@@ -88,7 +96,6 @@ public class OrderServiceImpl implements OrderService {
 //        this.memberRepository = memberRepository;
 //        this.discountPolicy = discountPolicy;
 //    }
-
 
 
     @Override

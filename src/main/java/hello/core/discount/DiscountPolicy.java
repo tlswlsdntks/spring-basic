6b8e601b46313b71c8d3ebdb1f255b26c12b771c
tlswlsdntks <tlswlsdntks@naver.com>
2022-12-23 19:45:33 +0900

    * 빈 등록(업무 시 자동/수동 구분)

--- src/main/java/hello/core/discount/DiscountPolicy.java
+++ src/main/java/hello/core/discount/DiscountPolicy.java
@@ -8,4 +8,16 @@ public interface DiscountPolicy {
      * @return 할인 대상 금액
      */
     int discount(Member member, int price);
+
+    /*
+        [빈 등록]
+        업무로직(화면개발): MVC 일반화 -> @Component 자동등록
+        기술로직(기술지원): 광범위 -> AppConfig.class처럼 수동등록
+         -> 자동등록을 원하면 특정패키지에서 작성
+
+        [정리]
+        편리한 자동기능을 기본으로 사용
+        직접등록하는 기술지원객체는 수동등록
+        다형성을 적극 활용하는 비즈니스 로직은 수동등록을 고민해보자
+     */
 }
